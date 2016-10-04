796b2676ba069a76f140dc17729f8c82840ae4a0
Jeffrey R Kuhn <drjrkuhn@gmail.com>
2016-10-04 13:41:51 -0500

    Initial working version

--- angora.rb
+++ angora.rb
@@ -1,35 +1,35 @@
 class Angora < Formula
   desc "A free finite-difference time-domain (FDTD) electromagnetic simulation package"
   homepage "http://www.angorafdtd.org/"
-  #url "http://www.angorafdtd.org/angora/angora-0.20.0.tar.gz"
-  version "0.20.0"
-  #sha256 "67a8473e17c6bb278b6610177ee863bb4684c5de55b2d683669184cc81d8cbcf"
+  url "https://github.com/drjrkuhn/angora/archive/0.20.1.tar.gz"
+  version "0.20.1"
+  sha256 "67a8473e17c6bb278b6610177ee863bb4684c5de55b2d683669184cc81d8cbcf"
   head "https://github.com/drjrkuhn/angora.git"
 
-  #depends_on :mpi => [:cc, :optional]
+   depends_on :mpi => [:cc, :recommended]
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "gettext" => :build
 
-  #mpi_args = []
-  #mpi_args << "with-mpi" if build.with? "mpi"
+  mpi_args = []
+  mpi_args << "with-mpi" if build.with? "mpi"
 
-  #depends_on "hdf5" => mpi_args
-  #depends_on "h5utils" => :recommended
-  #depends_on "blitz"
-  #depends_on "libconfig"
-  #depends_on "argp-standalone"
-  #depends_on "boost" >> mpi_args
+  depends_on "hdf5" => mpi_args
+  depends_on "h5utils" => :recommended
+  depends_on "blitz"
+  depends_on "libconfig"
+  depends_on "argp-standalone"
+  depends_on "boost" => mpi_args
   
   def install
-    #conf_args = [
-    #  "--disable-dependency-tracking",
-    #  "--disable-silent-rules",
-    #  "--prefix=#{prefix}"
-    #]
-    #conf_args << "--with-mpi" ? if build.with? "mpi"
+    conf_args = [
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    ]
+    conf_args << "--with-mpi" if build.with? "mpi"
     
     ## for testing
     # export CXXFLAGS="-std=c++98 -Wno-parentheses"
@@ -39,20 +39,20 @@ class Angora < Formula
     # export HDF5_CXX="c++" HDF5_CLINKER="c++"
 
     # default /usr/local/include search path
-    #ENV.append "CPLUS_INCLUDE_PATH", "-I#{HOMEBREW_PREFIX}/include"
-    #ENV.append "LIBRARY_PATH", "-I#{HOMEBREW_PREFIX}/lib"
+    ENV.append "CPLUS_INCLUDE_PATH", "-I#{HOMEBREW_PREFIX}/include"
+    ENV.append "LIBRARY_PATH", "-I#{HOMEBREW_PREFIX}/lib"
     # use older C++ standard
-    #ENV.append "CXXLAGS", "-std=c++98 -Wno-parentheses"
+    ENV.append "CXXLAGS", "-std=c++98 -Wno-parentheses"
     # force the library dependencies
-    #ENV.append "LIBS", "-largp -lhdf5 -lhdf5_cpp -lconfig++"
+    ENV.append "LIBS", "-largp -lhdf5 -lhdf5_cpp -lconfig++"
     # do not compile through the h5c++ script
-    #ENV.append "HDF5_CXX", "c++"
-    #ENV.append "HDF5_CLINKER", "c++"
+    ENV.append "HDF5_CXX", "c++"
+    ENV.append "HDF5_CLINKER", "c++"
 
 	
-    #system "autoreconf", "-fiv"
-    #system "./configure", *conf_args
-    #system "make", "install" # if this fails, try separate make/make install steps
+    system "autoreconf", "-fiv"
+    system "./configure", *conf_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   #test do
